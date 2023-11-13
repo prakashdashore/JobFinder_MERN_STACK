98871f2493ba0ef8cca50e1b216ba543f825ea2a
prakashdashore <prakashdashore716@gmail.com>
2023-11-13 12:15:22 +0530

    dot-env

--- backend/models/db.js
+++ backend/models/db.js
@@ -5,7 +5,6 @@ const connectDb = async ()=>{
     try {
         await mongoose.connect(process.env.MONGO_URL);
         console.log("db connnected...")
-        0.
         
     } catch (error) {
         console.log(error.message)
